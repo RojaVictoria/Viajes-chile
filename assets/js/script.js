d7d6236ad194325e569bebc8a36b810b1e94433a
RojaVictoria <82482780+RojaVictoria@users.noreply.github.com>
2022-04-08 20:45:36 -0400

    funcionalidad lista

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
 $(function () {
+    /*Activación de tooltips*/    
     $('[data-toggle="tooltip"]').tooltip()
+
+    /*Cambio de color con doble clic*/    
+    $("h3").on("dblclick", function(){
+        $(this).css({
+        "color": "#229eaa",
+        });
+    });
+
+    /*Toggle de tarjetas*/    
+    $(".title h5").click(function() {
+        $(".title p").toggle();
+    });
+
+    /*Alerta de mensaje enviado*/    
+    $("#enviarMensaje").click(function(){
+        alert("El mensaje fue enviado correctamente...");
+    });
+
 })
